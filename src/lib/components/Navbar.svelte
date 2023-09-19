318661c7525c65af873cfd56b1a438521b4f2e3f
rook1e <rook1e404@outlook.com>
2023-09-19 19:01:16 +0800

    fix: use pure svg icon
    https://github.com/tabler/tabler-icons/issues/838

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { IconMenu2, IconMoon, IconSun, IconX } from '@tabler/icons-svelte';
-
 	let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	$: {
 		let classList = document.documentElement.classList;
@@ -37,9 +35,31 @@
 				}}
 			>
 				{#if isNavbarOpen}
-					<IconX class="w-4 h-4" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-4 h-4"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
 				{:else}
-					<IconMenu2 class="w-4 h-4" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-4 h-4"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						/>
+					</svg>
 				{/if}
 			</button>
 		</div>
@@ -63,9 +83,29 @@
 				on:click={() => (darkMode = !darkMode)}
 			>
 				{#if darkMode}
-					<IconSun />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
+						/>
+					</svg>
 				{:else}
-					<IconMoon />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+							clip-rule="evenodd"
+						/>
+					</svg>
 				{/if}
 			</button>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,6 @@
 	import * as mmdb from 'mmdb-lib';
 	import { Buffer } from 'buffer';
 	import { onMount } from 'svelte';
-	import {
-		IconAlertTriangleFilled,
-		IconCircleCheckFilled,
-		IconCloudUpload,
-		IconDatabaseSearch
-	} from '@tabler/icons-svelte';
 	import MetadataItem from '$lib/components/MetadataItem.svelte';
 
 	let inputFiles: FileList;
@@ -95,7 +89,20 @@
 						: ''}"
 				>
 					<div class="flex flex-col items-center justify-center pt-5 pb-6">
-						<IconCloudUpload class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-8 h-8 mb-1 text-gray-500 dark:text-gray-400"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+							/>
+						</svg>
 						<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
 							<span class="font-semibold">Click to import</span> or drag and drop
 						</p>
@@ -107,14 +114,37 @@
 		</form>
 		{#if errorMsg !== ''}
 			<p class="flex items-center mt-2 text-sm text-red-600 dark:text-red-500">
-				<IconAlertTriangleFilled class="w-4 mr-1" />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					class="w-4 mr-1"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+						clip-rule="evenodd"
+					/>
+				</svg>
 				<span class="font-medium">Parsing failed.</span>
 				{errorMsg}
 			</p>
 		{/if}
 		{#if mmdbReader}
 			<p class="flex items-center mt-2 text-sm text-green-600 dark:text-green-500">
-				<IconCircleCheckFilled class="w-4 mr-1" />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					class="w-4 mr-1"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+
 				Successfully parsed [<span class="font-medium">{inputFiles[0].name}</span>].
 			</p>
 		{/if}
@@ -143,7 +173,18 @@
 				>
 				<div class="relative">
 					<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-						<IconDatabaseSearch class="w-5 text-gray-500 dark:text-gray-400" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							class="w-5 text-gray-500 dark:text-gray-400"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+								clip-rule="evenodd"
+							/>
+						</svg>
 					</div>
 					<input
 						type="search"
